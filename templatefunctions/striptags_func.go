d51374406f02cbc69e22f8488fbce2a05aeeb45a
Carsten Dietrich <carsten.dietrich@aoe.com>
2019-05-17 11:18:34 +0200

    templatefunctions: Add optional parameter to stripTags() template function
    This allows developers to output html safely as it will only output specific tags and their whitelisted attributes.
    
    As an example you can create a new list in `config.yml` and use it in your template with the config() function:
    
    ```yml
    allowedTags:
     - "h2"
     - "h3"
     - "p"
     - "a(href target rel)"
     - "img(src alt)"
    ```
    
    ```pug
    .productDescription = stripTags(description, config('allowedTags'))
    ```

--- templatefunctions/striptags_func.go
+++ templatefunctions/striptags_func.go
@@ -2,42 +2,132 @@ package templatefunctions
 
 import (
 	"context"
+	"regexp"
 	"strings"
 
+	"flamingo.me/flamingo/v3/framework/config"
 	"golang.org/x/net/html"
 )
 
 type (
-	StriptagsFunc struct{}
+	// StriptagsFunc provides template function to strip html tags
+	StriptagsFunc     struct{}
+	allowedAttributes map[string]struct{}
+	allowedTags       map[string]allowedTag
+	allowedTag        struct {
+		name       string
+		attributes allowedAttributes
+	}
 )
 
-// Func as implementation of debug method
+var (
+	nameRe          = regexp.MustCompile(`[a-z0-9\-]+`)
+	attributesRe    = regexp.MustCompile(`[a-z0-9]+([a-z]+)`)
+	selfClosingTags = map[string]struct{}{
+		"area":   {},
+		"base":   {},
+		"br":     {},
+		"col":    {},
+		"embed":  {},
+		"hr":     {},
+		"img":    {},
+		"input":  {},
+		"link":   {},
+		"source": {},
+	}
+)
+
+func createTag(definition string) allowedTag {
+	definition = strings.ToLower(definition)
+	attributes := make(allowedAttributes)
+	for _, attr := range attributesRe.FindAllString(definition, -1) {
+		attributes[attr] = struct{}{}
+	}
+
+	return allowedTag{
+		nameRe.FindString(definition),
+		attributes,
+	}
+}
+
+// Func implements the strip tags template function
 func (df StriptagsFunc) Func(ctx context.Context) interface{} {
-	return func(htmlString string) string {
+	return func(htmlString string, allowedTagsConfig ...config.Slice) string {
 		doc, err := html.ParseFragment(strings.NewReader(htmlString), nil)
 		if err != nil {
 			return ""
 		}
 
+		allowedTags := make(allowedTags)
+		if len(allowedTagsConfig) == 1 {
+			for _, item := range allowedTagsConfig[0] {
+				if definition, ok := item.(string); ok {
+					tag := createTag(definition)
+					allowedTags[tag.name] = tag
+				}
+			}
+		}
+
 		res := ""
 		for _, n := range doc {
-			res += removeTags(n)
+			res += cleanTags(n, allowedTags)
 		}
 		return res
 	}
 }
 
-func removeTags(n *html.Node) string {
+func cleanTags(n *html.Node, allowedTags allowedTags) string {
+	var allowedTag allowedTag
 	res := ""
+
+	if n.Type == html.ElementNode {
+		if tag, ok := allowedTags[n.Data]; ok {
+			allowedTag = tag
+		}
+	}
+
+	if allowedTag.name != "" {
+		res += "<"
+		res += n.Data
+		res += getAllowedAttributes(n.Attr, allowedTag.attributes)
+		if isSelfClosingTag(n) {
+			res += " /"
+		}
+		res += ">"
+	}
 
 	if n.Type == html.TextNode {
 		res += n.Data
 	}
+
 	if n.FirstChild != nil {
 		for c := n.FirstChild; c != nil; c = c.NextSibling {
-			res += removeTags(c)
+			res += cleanTags(c, allowedTags)
 		}
 	}
 
+	if allowedTag.name != "" && !isSelfClosingTag(n) {
+		res += "</" + n.Data + ">"
+	}
+
+	return res
+}
+
+func isSelfClosingTag(n *html.Node) bool {
+	if n.Type == html.ElementNode {
+		if _, ok := selfClosingTags[n.Data]; ok {
+			return true
+		}
+	}
+	return false
+}
+
+func getAllowedAttributes(attributes []html.Attribute, allowedAttributes allowedAttributes) string {
+	res := ""
+	for _, attr := range attributes {
+		if _, ok := allowedAttributes[attr.Key]; ok {
+			res += " " + attr.Key + "=\"" + html.EscapeString(attr.Val) + "\""
+		}
+	}
 	return res
 }
