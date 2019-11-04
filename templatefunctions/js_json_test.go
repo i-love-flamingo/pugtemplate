c5afb08a88b1ce1daaa6e72fd25537a1dc9d5618
Thomas Layh <thomas.layh@aoe.com>
2019-11-04 10:58:38 +0100

    fixed comments and added test for JSON Parse

--- templatefunctions/js_json_test.go
+++ templatefunctions/js_json_test.go
@@ -2,6 +2,7 @@ package templatefunctions
 
 import (
 	"context"
+	"flamingo.me/pugtemplate/pugjs"
 	"testing"
 
 	"flamingo.me/flamingo/v3/framework/flamingo"
@@ -13,4 +14,13 @@ func TestJsJSON(t *testing.T) {
 
 	json := jsJSON.Func(context.Background()).(func() JSON)()
 	assert.Equal(t, `{"foo":123}`, json.Stringify(map[string]int{"foo": 123}))
+}
+
+func TestJSON_Parse(t *testing.T) {
+	var jsJSON flamingo.TemplateFunc = new(JsJSON)
+
+	json := jsJSON.Func(context.Background()).(func() JSON)()
+	result := json.Parse(`{"foo":123}`)
+
+	assert.Implements(t, (*pugjs.Object)(nil), result)
 }
