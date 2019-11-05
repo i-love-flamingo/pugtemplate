52baa753fda43168e51089b5d28d09d6f76008a2
Thomas Layh <thomas.layh@aoe.com>
2019-11-05 14:54:02 +0100

    add template function to parse different types of values into an integer

---
+++ templatefunctions/parseInt_func_test.go
@@ -0,0 +1,72 @@
+package templatefunctions
+
+import (
+	"flamingo.me/pugtemplate/pugjs"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestParseInt_Func(t *testing.T) {
+	tests := []struct {
+		name  string
+		input interface{}
+		want  int
+	}{
+		{
+			name:  "empty string",
+			input: pugjs.String(""),
+			want:  0,
+		},
+		{
+			name:  "pugjs string",
+			input: pugjs.String("123"),
+			want:  123,
+		},
+		{
+			name:  " string",
+			input: "123",
+			want:  123,
+		},
+		{
+			name:  "pugjs number",
+			input: pugjs.Number(333),
+			want:  333,
+		},
+		{
+			name:  "float64",
+			input: float64(444),
+			want:  444,
+		},
+		{
+			name:  "float32",
+			input: float32(445),
+			want:  445,
+		},
+		{
+			name:  "int",
+			input: 555,
+			want:  555,
+		},
+		{
+			name:  "int32 - runs into default",
+			input: int32(321),
+			want:  321,
+		},
+		{
+			name:  "pugjs bool",
+			input: pugjs.Bool(true),
+			want:  0,
+		},
+		{
+			name:  "bool",
+			input: false,
+			want:  0,
+		},
+	}
+
+	for _, tt := range tests {
+		tmplFunc := new(ParseInt)
+		parseIntFunc := tmplFunc.Func().(func(o interface{}) int)
+		assert.Equal(t, tt.want, parseIntFunc(tt.input), "Testcase: %v - this values should be the same", tt.name)
+	}
+}
